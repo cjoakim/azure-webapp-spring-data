065ba94dd407cc6640d14b5113b653ca0e677f60
Chris Joakim <christopher.joakim@gmail.com>
2020-01-06 07:57:04 -0500

    Removed cats - ddl, model, repository, controller

--- admin/api-curls.sh
+++ admin/api-curls.sh
@@ -5,18 +5,6 @@ base_url="http://cjoakim-spring-data.azurewebsites.net:80"
 sleep_sec=5
 
 echo '=========='
-echo 'Invoking cats/ endpoints ...'
-
-curl -v $base_url"/cats/findAll"
-sleep $sleep_sec
-
-curl -v $base_url"/cats/findById/1"
-sleep $sleep_sec
-
-curl -v $base_url"/cats/findByName/Elsa"
-sleep $sleep_sec
-
-echo '=========='
 echo 'Invoking airports/ endpoints ...'
 
 curl -v $base_url"/airports/findAll"
